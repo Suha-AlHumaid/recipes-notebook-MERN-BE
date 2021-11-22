241fa7eb181d02a9299b59b38ce9eacd7f17b2d1
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-22 09:40:34 +0300

    Schema

---
+++ db/models/userSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  userName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: Number },
+  isDeleted: { type: Boolean, default: false },
+  publishedRecpie: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'recipeSchema'
+ }]
+},
+{timestamps: true});
+
+module.exports = mongoose.model("userSchema", userSchema);
